b6e6be8d9dffc807368100d3cfbcb8a9901ba3f4
ShadyObeyd <obeyd.shady@gmail.com>
2018-03-27 19:36:09 +0300

    Added 05.IteratorsAndComparators-Lab

---
+++ 05.IteratorsAndComparators-Lab/03.ComparableBook/Library.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Library : IEnumerable<Book>
+{
+    private SortedSet<Book> books;
+
+    public Library(params Book[] books)
+    {
+        this.books = new SortedSet<Book>(books);
+    }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        return new LibraryIterator(this.books);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    private class LibraryIterator : IEnumerator<Book>
+    {
+        private List<Book> books;
+        int index;
+
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            this.Reset();
+            this.books = new List<Book>(books);
+        }
+
+        public Book Current => this.books[index];
+
+        object IEnumerator.Current => this.Current;
+
+        public void Dispose()
+        {
+
+        }
+
+        public bool MoveNext()
+        {
+            this.index++;
+            return this.index < this.books.Count;
+        }
+
+        public void Reset()
+        {
+            this.index = -1;
+        }
+    }
+}
